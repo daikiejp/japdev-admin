b1ed9e11e3e682c6403774e9831a03d7273ffdca
Danny Davila <danny@misaki.tech>
2025-10-28 03:20:34 +0900

    chore: RGB HEXを6文字に統一し、Viewに透過設定を追加

--- app/models/devsheet_category.rb
+++ app/models/devsheet_category.rb
@@ -3,7 +3,7 @@ class DevsheetCategory < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true
-  validates :color, format: { with: /\A#[0-9A-Fa-f]{8}\z/, message: "は有効なHEXカラーコードである必要があります" }, allow_blank: true
+  validates :color, format: { with: /\A#[0-9A-Fa-f]{6}\z/, message: "は有効なHEXカラーコードである必要があります" }, allow_blank: true
 
   before_validation :generate_slug, if: -> { slug.blank? && name.present? }
 
